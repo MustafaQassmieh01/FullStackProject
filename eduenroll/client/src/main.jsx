b235ab79f58834f20f78c002f5e0d294ea01d68f
Mustafa.Qassmieh01 <mustafqasmieh@gmail.com>
2025-10-08 11:57:01 +0200

    feature: made components for the admin dashbord

--- eduenroll/client/src/main.jsx
+++ eduenroll/client/src/main.jsx
@@ -3,12 +3,19 @@ import React from 'react';
 import App from './App.jsx'
 import { AuthProvider } from './core/context/authProvider.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import CourseDisplay from './Admin/dashboard/components/CourseDisplay.jsx';
+import UserDisplay from './Admin/dashboard/components/UserDisplay.jsx';
+import RegistrationDisplay from './Admin/dashboard/components/RegistrationDisplay.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        {/* <App /> */}
+        <CourseDisplay/>
+        <UserDisplay />
+        <RegistrationDisplay />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
